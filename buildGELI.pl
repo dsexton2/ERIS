cf9b51cf9490ed775a2517c8a9b7ff8d2654ba9f
30133:32719:p-qc <p-qc@sug-login1.(none)>
2011-07-05 15:13:31 -0500

    Utilize General::Config, add perlpod.
    I made use of the General::Config module to use configuration files, to
    avoid hard-coding file paths and parameters.  I also added perlpod
    markup for documentation purposes.  Finally, I cleaned up the strings
    defining the JAR commands for legibility and maintenance - one
    parameter/value pair per line.

--- buildGELI.pl
+++ buildGELI.pl
@@ -1,7 +1,32 @@
 #! /usr/bin/perl -w
 
+=head1 NAME
+
+buildGELI
+
+=head1 SYNOPSIS
+
+perl buildGELI.pl
+
+=head1 DESCRIPTION
+
+This script converts all .birdseed.data.txt files in the target directory into .bs, using .geli as an intermediate format.  It does this by calling two JARs, which each accomplish one of the conversions.
+
+=head1 LICENSE
+
+This script is the property of Baylor College of Medicine.
+
+=head1 AUTHOR
+
+Updated by John McAdams - L<mailto:mcadams@bcm.edu>
+
+=cut
+
 use strict;
+use Config::General;
 use Log::Log4perl;
+
+my %config = new Config::General("tertiary_pipeline.cfg")->getall;
 
 Log::Log4perl->init("log4perl.cfg");
 my $errorLog = Log::Log4perl->get_logger("errorLogger");
@@ -15,18 +40,24 @@ if ($size == 0) { $errorLog->error("no *.birdseed.data.txt files found in local 
 my $cmd = '';
 foreach my $file (@files) {
   # covert cancer birdseed data files to geli format
-  $cmd = "/users/bainbrid/bin/java -jar /stornext/snfs0/next-gen/yw14-scratch/Birdseed_converter/CancerBirdseedSNPsToGeli.jar I=$file S=TCGA-sample SNP60_DEFINITION=/stornext/snfs0/next-gen/yw14-scratch/Birdseed_converter/GenomeWideSNP_6.na26.1.annot.csv SD=/stornext/snfs0/next-gen/yw14-scratch/Birdseed_converter/Homo_sapiens_assembly18.dict R=/stornext/snfs0/next-gen/yw14-scratch/Birdseed_converter/Homo_sapiens_assembly18.fasta O=$file.geli";
-  #print "building .geli for $file\n";
+  $cmd = $config{"java"}." -jar ".$config{"CancerBirdseedSNPsToGeliJAR"}.
+	" I=$file".
+	" S=".$config{"SAMPLE"}.
+	" SNP60_DEFINITION=".$config{"SNP60_DEFINITION"}.
+	" SD=".$config{"SEQUENCE_DICTIONARY"}.
+	" R=".$config{"REFERENCE"}.
+	" O=$file.geli";
   $debugLog->debug("building .geli for $file\n");
-  #print "$cmd\n";
   $debugLog->debug("$cmd\n");
   system("$cmd");
 
   # build bs file from geli
-  $cmd = "/users/bainbrid/bin/java -jar /stornext/snfs0/next-gen/yw14-scratch/Birdseed_converter/GeliToTextExtended.jar OUTPUT_LIKELIHOODS=False I=$file.geli >& $file.bs";
-  #print "building .bs for $file\n";
+  $cmd = $config{"java"}." -jar ".$config{"GeliToTextExtendedJAR"}.
+	" OUTPUT_LIKELIHOODS=".$config{"OUTPUT_LIKELIHOODS"}.
+	" I=$file.geli".
+	" >& ".
+	" $file.bs";
   $debugLog->debug("building .bs for $file\n");
   $debugLog->debug("$cmd\n");
-  #print "$cmd\n";
   system("$cmd");
 }
