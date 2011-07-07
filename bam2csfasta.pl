2127238c3b77c812c5fc1d836408429d87ab7d65
30133:32719:p-qc <p-qc@sug-login1.(none)>
2011-07-07 08:37:43 -0500

    Fixed out-of-sequence file name substitution, added log paths.
    The script was replacing part of the .bam file path before calling the
    JAR, which resulted in a file not found error.  The substitution was
    moved to directly before the print to the resultant CSV file whose
    content necessitates the substitution in the first place.
    
    The log paths were also made absolute (although these should eventually
    be put in the configuration file - hard-coded paths should be avoided in
    source code altogether).

--- bam2csfasta.pl
+++ bam2csfasta.pl
@@ -30,9 +30,9 @@ use warnings;
 use Config::General;
 use Log::Log4perl;
 
-my %config = new Config::General("tertiary_pipeline.cfg")->getall;
+my %config = new Config::General("/users/p-qc/testenv/tertiary_pipeline.cfg")->getall;
 
-Log::Log4perl->init("log4perl.cfg");
+Log::Log4perl->init("/users/p-qc/testenv/log4perl.cfg");
 my $error_log = Log::Log4perl->get_logger("errorLogger");
 my $debug_log = Log::Log4perl->get_logger("debugLogger");
 
@@ -53,15 +53,15 @@ while (<CSV_FILE_BAM>) {
 		print "Bad sample_id/input_bam_file pair: $sample_id\t$input_bam_file\n";
 		next;
 	}
-	$input_bam_file =~ s/\.sorted\.dups\.rg//g;
 	(my $output_csfasta_file = $input_bam_file) =~ s/bam$/csfasta/g;
 	my $command = $config{"java"}." -Xmx2G -jar ".$config{"bam2csfastaJAR"}.
 		" $input_bam_file".
 		" >".
 		" $output_csfasta_file";
 	$debug_log->debug("Executing command: $command\n");
+	$input_bam_file =~ s/\.sorted\.dups\.rg//g;
 	print CSV_FILE_CSFASTA "$sample_id,$output_csfasta_file\n";
-	#system($command);
+	system($command);
 }
 
 close(CSV_FILE_BAM);
