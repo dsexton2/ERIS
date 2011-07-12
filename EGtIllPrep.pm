21b0af0741144ac990d79642f78e8d001f5ec81e
30133:32719:p-qc <p-qc@sug-login1.(none)>
2011-07-12 07:34:14 -0500

    Finished wrapping into module.
    Added accessor/mutators for the input/output file paths.  Wrapped the
    main logic in a function.

--- EGtIllPrep.pm
+++ EGtIllPrep.pm
@@ -4,85 +4,93 @@ package Concordance::EGtIllPrep;
 
 use strict;
 use warnings;
+use Config::General;
 use Log::Log4perl;
-
-# /stornext/snfs0/next-gen/Illumina/Instruments/700738/110330_SN738_0062_AB05LKABXX/Data/Intensities/BaseCalls/GERALD_09-04-2011_p-illumina.8
-# /stornext/snfs5/next-gen/Illumina/Instruments/700166/100818_SN166_0135_A20AA7ABXX/mini_analysis/lane7
 
 my $error_log = Log::Log4perl->get_logger("errorLogger");
 my $debug_log = Log::Log4perl->get_logger("debugLogger");
 
 sub new {
 	my $self = {};
-	$self->{CONFIG} = ();
+	$self->{INPUT_CSV_PATH} = undef;
+	$self->{OUTPUT_TXT_PATH} = undef;
 	bless($self);
 	return $self;
 }
 
-sub config {
+sub input_csv_path {
 	my $self = shift;
-	if (@_) { %{ $self->{CONFIG} } = @_; }
-	return %{ $self->{CONFIG} };
+	if (@_) { $self->{INPUT_CSV_PATH} = shift; }
+	return $self->{INPUT_CSV_PATH};
 }
 
-
-
-my $inFile = $ARGV[0];
-open(inFP,"<$inFile");
-while (<inFP>) {
-  chomp($_);
-  my $current_line = $_;
-
-  my @data = split(/[,\s]/,$current_line);
-  my $NAME = $data[0];
-  my $PATH = $data[1];
-#  print "\n$data[0]\n";  
-$debug_log->debug("data[0]: ",$data[0],"\n");
-chomp($PATH);
-
-  my @dirs = split(/\//,$PATH);
-  my $instrument = $dirs[6];
-
-  my @fcra = split(/_/,$dirs[7]);
-  my $flowcell = $fcra[3];
-
-  my $SE = '';
-  my $analysis_number = 0;
-  if ($PATH =~ /Demultiplexed/) {
-    my $barcode = $dirs[12];
-    $dirs[13] =~ /(\d+)$/;
-    $analysis_number = $1; 
-    #my @grld = split(/\./,$dirs[13]);
-    #$analysis_number = $grld[1];
-    if ($analysis_number eq '') { $analysis_number = 1; }    
-    $SE = "${flowcell}_${barcode}_${analysis_number}";
-  } else {
-    if ($dirs[11] =~ /GERALD/) {
-      my @grld = split(/\./,$dirs[11]);
-      $analysis_number = $grld[1];
-    } elsif ($dirs[9] =~ /lane(\d+)/) {
-      $analysis_number = $1;
-    }
-    if ($analysis_number eq '') { $analysis_number = 1; }
-    $SE = "${flowcell}_${analysis_number}";
-  }
-
-  ### print "\n|${PATH}|\n";
-  ### print ":${NAME}_${SE}:\n";
-
-  my @files = `ls $PATH/*sequence.txt.bz2`;
-
-  if ($files[0] ne '') {
-    #print "${NAME}_${SE}";
-    $debug_log->debug("{NAME}_${SE}:${NAME}_${SE}\n");
-    foreach my $fastq (@files) {
-      chomp($fastq);
-      #print "\t$fastq";
-      $debug_log->debug("fastq:\s",$fastq,"\n");
-    }
-    #print "\n";
-  } else {
-    #print STDERR "ERROR :: $PATH/*sequence.txt.bz2 not found for ${NAME}_${SE}\n";
-    $error_log->error("$PATH/*sequence.txt.bz2 not found for ${NAME}_${SE}\n");
-  }
+sub output_txt_path {
+	my $self = shift;
+	if (@_) { $self->{OUTPUT_TXT_PATH} = shift; }
+	return $self->{OUTPUT_TXT_PATH};
 }
+
+sub generate_fastq_list {
+	my $self = shift;
+	#my %config = $self->config;
+	my $inFile = $self->input_csv_path;
+	open(inFP,"<$inFile");
+	open (OUTFILE, "> ".$self->output_txt_path);
+	while (<inFP>) {
+		chomp($_);
+		my $current_line = $_;
+	
+		my @data = split(/[,\s]/,$current_line);
+		my $NAME = $data[0];
+		my $PATH = $data[1];
+		print OUTFILE "$data[0]";  
+		chomp($PATH);
+	
+		my @dirs = split(/\//,$PATH);
+		my $instrument = $dirs[6];	
+		#my @fcra = ();
+		#foreach my $split_dir (@dirs) {
+		#	if ($split_dir =~ /\w+_/) {
+		#		@fcra = split(/_/, $split_dir);
+		#		last;
+		#	}
+		#}
+		my @fcra = split(/_/,$dirs[7]);
+		
+		my $flowcell = $fcra[3];
+	
+		my $SE = '';
+		my $analysis_number = 0;
+		if ($PATH =~ /Demultiplexed/) {
+			my $barcode = $dirs[12];
+			$dirs[13] =~ /(\d+)$/;
+			$analysis_number = $1; 
+			if ($analysis_number eq '') { $analysis_number = 1; }    
+			$SE = "${flowcell}_${barcode}_${analysis_number}";
+		} else {
+			if ($dirs[11] =~ /GERALD/) {
+				my @grld = split(/\./,$dirs[11]);
+				$analysis_number = $grld[1];
+			} elsif ($dirs[9] =~ /lane(\d+)/) {
+				$analysis_number = $1;
+			}
+			if ($analysis_number eq '') { $analysis_number = 1; }
+			$SE = "${flowcell}_${analysis_number}";
+		}
+	
+		my @files = `ls $PATH/*sequence.txt.bz2`;
+	
+		if ($files[0] ne '') {
+			print OUTFILE "${NAME}_${SE}";
+			foreach my $fastq (@files) {
+				chomp($fastq);
+				print OUTFILE "\t$fastq";
+			}
+			print OUTFILE "\n";
+		} else {
+			$error_log->error("$PATH/*sequence.txt.bz2 not found for ${NAME}_${SE}\n");
+		}
+	}
+}
+
+1;
