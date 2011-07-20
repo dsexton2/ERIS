734b313b24528bf041ac1243c03b0c360d6b69da
30133:32719:p-qc <p-qc@sug-login1.(none)>
2011-07-20 14:10:18 -0500

    Changed configuration calls, moved perldoc.
    Changed configuration calls to reflect new name convention.  Moved
    perldoc to bottom of file.  Added module return value.

--- BuildGeliAndBS.pm
+++ BuildGeliAndBS.pm
@@ -1,58 +1,5 @@
 #! /usr/bin/perl -w
 
-=head1 NAME
-
-Concordandce::BuildGeliAndBS - converts .birdseed.data.txt to .bs
-
-=head1 SYNOPSIS
-
- use Concordance::BuildGeliAndBS;
- my $build_Geli_and_BS = Concordance::BuildGeliAndBS->new;
- $build_Geli_and_BS->config(%config);
- $build_Geli_and_BS->path("/foo/bar/");
- $build_Geli_and_BS->build_geli;
- $build_Geli_and_BS->build_bs;
-
-=head1 DESCRIPTION
-
-This script converts all .birdseed.data.txt files in the target directory into .bs, using .geli as an intermediate format.  It does this by calling two JARs, which each accomplish one of the conversions.
-
-=head2 Methods
-
-=over 12
-
-=item C<new>
-
-Returns a new Concordance::BuildGeliAndBS object.
-
-=item C<config>
-
-Gets or sets and gets the General::Config object.
-
-=item C<path>
-
-Gets or sets and gets the path string in which to search for .birdseed.data.txt or .geli files.
-
-=item C<build_geli>
-
-Calls a JAR to convert each .birdseed.data.txt file to a .geli file.  
-
-=item C<build_bs>
-
-Calls a JAR to convert each .geli file into a .bs file.
-
-=back
-
-=head1 LICENSE
-
-This script is the property of Baylor College of Medicine.
-
-=head1 AUTHOR
-
-Updated by John McAdams - L<mailto:mcadams@bcm.edu>
-
-=cut
-
 package Concordance::BuildGeliAndBS;
 
 use strict;
@@ -105,12 +52,12 @@ sub build_geli {
 
 	foreach my $file (@files) {
 	# covert cancer birdseed data files to geli format
-		$cmd = $config{"java"}." -jar ".$config{"CancerBirdseedSNPsToGeliJAR"}.
+		$cmd = $config{"java"}." -jar ".$config{"cancer_birdseed_snps_to_geli_jar"}.
 		" I=$file".
-		" S=".$config{"SAMPLE"}.
-		" SNP60_DEFINITION=".$config{"SNP60_DEFINITION"}.
-		" SD=".$config{"SEQUENCE_DICTIONARY"}.
-		" R=".$config{"REFERENCE"}.
+		" S=".$config{"sample_name"}.
+		" SNP60_DEFINITION=".$config{"snp60_definition_path"}.
+		" SD=".$config{"sequence_dictionary_path"}.
+		" R=".$config{"reference_path"}.
 		" O=$file.geli";
 		$debug_log->debug("building .geli for $file\n");
 		$debug_log->debug("$cmd\n");
@@ -126,8 +73,8 @@ sub build_bs {
 
 	foreach my $file (@files) {
 		# build bs file from geli
-		$cmd = $config{"java"}." -jar ".$config{"GeliToTextExtendedJAR"}.
-			" OUTPUT_LIKELIHOODS=".$config{"OUTPUT_LIKELIHOODS"}.
+		$cmd = $config{"java"}." -jar ".$config{"geli_to_text_extended_jar"}.
+			" OUTPUT_LIKELIHOODS=".$config{"output_likelihoods"}.
 			" I=$file".
 			" >& ".
 			" $file.bs";
@@ -136,3 +83,58 @@ sub build_bs {
 		system("$cmd");
 	}
 }
+
+1;
+
+=head1 NAME
+
+Concordandce::BuildGeliAndBS - converts .birdseed.data.txt to .bs
+
+=head1 SYNOPSIS
+
+ use Concordance::BuildGeliAndBS;
+ my $build_Geli_and_BS = Concordance::BuildGeliAndBS->new;
+ $build_Geli_and_BS->config(%config);
+ $build_Geli_and_BS->path("/foo/bar/");
+ $build_Geli_and_BS->build_geli;
+ $build_Geli_and_BS->build_bs;
+
+=head1 DESCRIPTION
+
+This script converts all .birdseed.data.txt files in the target directory into .bs, using .geli as an intermediate format.  It does this by calling two JARs, which each accomplish one of the conversions.
+
+=head2 Methods
+
+=over 12
+
+=item C<new>
+
+Returns a new Concordance::BuildGeliAndBS object.
+
+=item C<config>
+
+Gets or sets and gets the General::Config object.
+
+=item C<path>
+
+Gets or sets and gets the path string in which to search for .birdseed.data.txt or .geli files.
+
+=item C<build_geli>
+
+Calls a JAR to convert each .birdseed.data.txt file to a .geli file.  
+
+=item C<build_bs>
+
+Calls a JAR to convert each .geli file into a .bs file.
+
+=back
+
+=head1 LICENSE
+
+This script is the property of Baylor College of Medicine.
+
+=head1 AUTHOR
+
+Updated by John McAdams - L<mailto:mcadams@bcm.edu>
+
+=cut
