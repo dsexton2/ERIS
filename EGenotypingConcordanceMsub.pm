0859f0a17b52c91692bd2a92b2948a1dc97033fd
30133:32719:p-qc <p-qc@sug-login1.(none)>
2011-07-25 09:14:37 -0500

    Initial commit.

---
+++ EGenotypingConcordanceMsub.pm
@@ -0,0 +1,76 @@
+#! /usr/bin/perl -w
+
+package Concordance::EGenotypingConcordanceMsub;
+
+use strict;
+use warnings;
+
+use Log::Log4perl;
+use Inline Ruby => 'require "/stornext/snfs5/next-gen/Illumina/ipipe/lib/Scheduler"';
+
+my $error_log = Log::Log4perl->get_logger("errorLogger");
+my $debug_log = Log::Log4perl->get_logger("debugLogger");
+
+sub new {
+	my $self = {};
+	$self->{EGENOLIST} = undef;
+	$self->{SNPARRAY} = undef;
+	bless($self);
+	return $self;
+}
+
+sub e_geno_list {
+	my $self = shift;
+	if (@_) { $self->{EGENOLIST} = shift }
+	return $self->{EGENOLIST};
+}
+
+sub snp_array {
+	my $self = shift;
+	if (@_) { $self->{SNPARRAY} = shift }
+	return $self->{SNPARRAY};
+}
+
+sub execute {
+	my $self = shift;
+	open(FIN, $self->e_geno_list);
+	my $i=1;
+	my @com_array;
+	while(<FIN>)
+	{
+		chomp;
+		my @a=split(/\s+/);
+		$temp=join("#",@a);
+		#$command = "bsub -e $i.e -o $i.o -J $i\_eGeno\_concor.job \"/stornext/snfs0/next-gen/concordance_analysis/e-Genotyping_concordance.pl $temp $SNP_array \"\;";
+		my $command = "\"/stornext/snfs0/next-gen/concordance_analysis/e-Genotyping_concordance.pl $temp ".$self->snp_array."  " \"\;";
+		
+		my $scheduler = new Concordance::Bam2csfasta::Scheduler("$i\_eGeno\_concor.job", $command);
+		$scheduler->setMemory(2000);
+		$scheduler->setNodeCores(2);
+		$scheduler->setPriority('normal');
+		$debug_log->debug("Submitting job with command: $command\n");
+		$scheduler->runCommand;
+
+		$com_array[$i] = $command;
+		$i++;
+	}
+	close(FIN);
+}
+
+1;
+
+=head1 NAME
+
+Concordance::EGenotypingConcordanceMsub
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 LICENSE
+
+=head1 AUTHOR
+
+Updated by John McAdams - L<mailto:mcadams@bcm.edu>
+
+=cut
