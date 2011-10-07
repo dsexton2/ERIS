4261311d07f04cd7286e48b9b660dc1e95df05f8
p-qc <p-qc@stornext7.(none)>
2011-10-07 10:56:38 -0500

    Removed unused methods, LIMS webservice just uses run IDs.
    Also added perldoc.  LIMS was enhanced to return the SNP array names as
    well as part of the webservice, so only a list of run IDs needs to be
    passed in.

--- Utils.pm
+++ Utils.pm
@@ -10,77 +10,28 @@ my $error_log = Log::Log4perl->get_logger("errorLogger");
 my $debug_log = Log::Log4perl->get_logger("debugLogger");
 my $warn_log = Log::Log4perl->get_logger("warnLogger");
 
-sub get_params {
-	# reads in the parameters from the metadata, and parses the data in
-	# as a hash of hashes, with the following structure:
-	# %hash = { class_name => %hash2 { param => validating_regex } }
-	my $self = shift;
-	my %config = undef;
-	my %pipeline_params = undef;
-	if (@_) { %config = shift }
-	if (@_) { %pipeline_params = shift }
-	if (-e $config{"metadata_file"}) {
-		open(FIN, $config{"metadata_file"});
-		my @lines = <FIN>;
-		my $key = undef;
-		foreach my $line (@lines) {
-			chomp($line);
-			if ($line =~ m/^\w/) {
-				$key = $line;
-				$pipeline_params{$key} = ();
-				next;
-			}
-			$line =~ m/\t(.*)\t(.*)$/;
-			$pipeline_params{$key}{$1} = "$2";
-		}
-		close(FIN);
-	}
-	return %pipeline_params;
-}
+=head1 NAME
 
-sub read_and_validate_input {
-	my $self = shift;
-	my $prompt = undef;
-	my $validation_pattern = undef;
-	if (@_) {
-		$prompt = shift;
-		$validation_pattern = shift;
-	}
-	my $term = Term::ReadLine->new("");
-	my $input = $term->readline($prompt.": ");
-	chomp($input);
-	while ($input !~ /$validation_pattern/) {
-		print "$input failed validation on $validation_pattern\n";
-		$input = $term->readline($prompt);	
-	}
-	$self->add_to_hash($prompt, $input);
-	return $input;
-}
+Concordance::Utils - common functions
 
-sub add_to_hash {
-	if (@_) {
-		my $self = shift;
-		my $key = shift;
-		my $value = shift;
-		$self->{CONFIG}{$key} = $value;
-		$self->write_config_file;
-	}
-}
+=head1 SYNOPSIS
 
-sub write_config_file {
-	my $self = shift;
-	open(CONFIG_OUT, ">".$self->config_file) || die "Couldn't open file for writing configuration items: ".$self->config_file."\n";
-	my %config = $self->config;
-	foreach my $key (sort (keys %config)) {
-		print CONFIG_OUT $key." = ".$config{$key}."\n";
-	}
-	close CONFIG_OUT;
-}
+ my @file_list = Concordance::Utils->get_file_list($path, $ext);
+ my %samples = Concordance::Utils->populate_sample_info_hash($run_list);
 
-sub check_perms {
-	# ensure that user@host has write permissions for path
-	
-}
+=head1 DESCRIPTION
+
+=head2 Methods
+
+=head3 get_file_list
+
+ my $path = "/foo/bar";
+ my $ext = "foo";
+ my @file_list = Concordance::Utils->get_file_list($path, $ext);
+
+For a given path and file extension, returns all files of that type from the directory as an array.
+
+=cut
 
 sub get_file_list {
 	# returns a list of all files of type $file_extension from directory $path
@@ -97,52 +48,54 @@ sub get_file_list {
 	return @files;
 }
 
-sub validate_param {
-	my $validation_regex = undef;
-	my $path = undef;
-	if ($#_ <= 1) {  }
-	if (@_) { $validation_regex = shift }
-	if (@_) { $path = shift }
-	if ($path !~ /$validation_regex/) { }
-	if (-e $path) { 
-		if (!-r $path) { $error_log->error("\n") }
-		if (!-w $path) { $error_log->error("\n") }
-	}
-	else { $warn_log->warn("$path DNE\n") }
-}
+=head3 populate_sample_info_hash
+
+ my $run_id_list = "foo,bar,rab,oof";
+ my %samples = Concordance::Utils->populate_sample_info_hash($run_id_list);
+
+For a given comma-delimited list of run IDs, executes a query against LIMS via curl, returning the associated sample ID, SNP array, and result path, storing each into a Sample object, and returning a hash of sample ID => Sample.
+
+=cut
 
 sub populate_sample_info_hash {
 	shift;
 	# run_id_list - this should be a comma-delimited list of run IDs
 	# returns a hash populated as hash{sample_id} => Concordance::Sample
-	my %runids_to_snps = ();
-	if (@_) { %runids_to_snps = @_ } 
-
 	my $run_id_list = "";
-	foreach my $run_id (keys %runids_to_snps) { $run_id_list .= $run_id."," }
-	$run_id_list =~ s/(.*),$/$1/;
+	if (@_) { $run_id_list = shift } 
 
 	my $post_data = "runnamelist=$run_id_list";
 	my $url = "http://gen2.hgsc.bcm.tmc.edu/gen2lims-reporting/jaxrs/reportservice/runinfo";
 	my $result = `curl -d "$post_data" -X POST $url`;
 
 	my %samples = ();
-	while ($result =~ m/([^,]+),([^,]+),(.*)\n/) {
-	    if ($1 ne "run_name") {
-	        $samples{$2} = Concordance::Sample->new;
-	        $samples{$2}->run_id($1);
-			$samples{$2}->snp_array($runids_to_snps{$1});
-	        $samples{$2}->sample_id($2);
-	        $samples{$2}->result_path($3);
-	    }   
-	    $result =~ s/$1,$2,$3\n//; # remove the line; finished processing it
+	while ($result =~ m/([^,]+),([^,]+),([^,]+),(.*)\n/) {
+		my $run_id = $1;
+		my $sample_id = $2;
+		my $orig_sample_id = $2;
+		my $result_path = $3;
+		my $snp_array = $4;
+		$sample_id =~ s/^HCC-//;
+		if ($run_id ne "run_name") {
+			$samples{$sample_id} = Concordance::Sample->new;
+			$samples{$sample_id}->run_id($run_id);
+			$samples{$sample_id}->snp_array($snp_array);
+			$samples{$sample_id}->sample_id($sample_id);
+			$samples{$sample_id}->result_path($result_path);
+		}
+	    $result =~ s/$run_id,$orig_sample_id,$result_path,$snp_array\n//; # remove the line; finished processing it
 	}
-	print %samples;
 	return %samples;
 }
 
-#stubs
+=head1 LICENSE
 
-#
+GPLv3.
+
+-head1 AUTHOR
+
+John McAdams - L<mailto:mcadams@bcm.edu>
+
+=cut
 
 1;
