e8bea40d2324ae645d7d113c0299664ecd70aa75
p-qc <p-qc@stornext7.(none)>
2011-10-11 14:15:39 -0500

    Initial commit.

---
+++ Frequency/join_partial_prefrequency_probelists.pl
@@ -0,0 +1,10 @@
+use File::Slurp;
+
+my @files = glob("*.prefrequency_probelist.part");
+
+my $outfile = "prefrequency_probelist";
+
+foreach my $file (@files) {
+	my $text = read_file($file);
+	write_file( $outfile, {append => 1 }, $text ) ; 
+}
