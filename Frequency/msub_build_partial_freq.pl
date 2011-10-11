8f972eff185b0593fb5c50ba6b78c7e7c8c7daaa
p-qc <p-qc@stornext7.(none)>
2011-10-11 11:30:13 -0500

    Added check to ensure an argument was passed.

--- Frequency/msub_build_partial_freq.pl
+++ Frequency/msub_build_partial_freq.pl
@@ -3,16 +3,15 @@
 use strict;
 use warnings;
 use diagnostics;
-
 use Inline Ruby => 'require "/stornext/snfs5/next-gen/Illumina/ipipe/lib/Scheduler.rb"';
+
+if ($#ARGV != 0) { die "Usage: perl msub_build_partial_freq.pl /path/to/xml/file.xml\n" }
 
 my $command = "/users/p-qc/dev/Concordance/Frequency/build_partial_freq.pl ".$ARGV[0];
 (my $job_name = $ARGV[0]) =~ s/.*\/([^\/]+)\.xml/$1/;
 
 my $scheduler = new Scheduler($job_name, $command);
-
 $scheduler->setMemory(28000);
 $scheduler->setNodeCores(2);
 $scheduler->setPriority('normal');
 $scheduler->runCommand;
-
