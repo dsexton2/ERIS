b1b4020312b4ed32ccb25875e850eeb55e17b332
p-qc <p-qc@stornext7.(none)>
2011-10-12 13:29:20 -0500

    Processes inputs of probelist and prefrequency probelist.

--- Frequency/msub_hapmap.pl
+++ Frequency/msub_hapmap.pl
@@ -5,7 +5,13 @@ use warnings;
 use diagnostics;
 use Inline Ruby => 'require "/stornext/snfs5/next-gen/Illumina/ipipe/lib/Scheduler.rb"';
 
-my $command = "/users/p-qc/dev/Concordance/Frequency/hapmap.pl ";
+if ($#ARGV != 1) { die "usage: perl msub_hapmap.pl /path/to/prefrequency/probelist /path/to/output/probelist\n" }
+
+my $prefrequency_probelist = $ARGV[0];
+my $probelist = $ARGV[1];
+
+my $command = "/users/p-qc/dev/Concordance/Frequency/hapmap.pl ".
+	$prefrequency_probelist." ".$probelist;
 my $job_name = "add_freq_to_probelist";
 
 my $scheduler = new Scheduler($job_name, $command);
