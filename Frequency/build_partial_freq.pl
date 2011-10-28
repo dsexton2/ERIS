c6260f2d643b3bbe3c3885e7f969d744f3ec3acc
p-qc <p-qc@stornext7.(none)>
2011-10-28 13:56:45 -0500

    Changed processing of 3' and 5' sequences.
    Using an external file, b134_SNPChrPosOnRef_37_2.bcp, to find out if the
    strands are reversed or not, then take the appropriate action on the
    sequences obtained from the Sequence/exemplarSs node. This drastically
    simplifies the logic for this part.

--- Frequency/build_partial_freq.pl
+++ Frequency/build_partial_freq.pl
@@ -11,6 +11,22 @@ if (!-e $ARGV[0]) { die "Input file ".$ARGV[0]." DNE!\n" }
 my %field_values;
 my @field_order = qw(chromosome chr_loc rsId Seq5 Seq3 ref_allele var_allele);
 
+my %orientation_values;
+
+# read in sequence orientation information
+open(FIN, "/stornext/snfs5/next-gen/concordance_analysis/dbSNP/hg19/b134_SNPChrPosOnRef_37_2.bcp") or die $!;
+while (<FIN>) {
+	chomp;
+	my @columns = split(/\t/, $_);
+	my $rsId = "rs".$columns[0];
+	$orientation_values{$rsId}{"chromosome"} = $columns[1];
+	$orientation_values{$rsId}{"position"} = $columns[2];
+	$orientation_values{$rsId}{"isReverse"} = $columns[3];
+}
+close(FIN);
+
+print STDOUT "read ".(scalar keys %orientation_values)." items from b134_SNPChrPosOnRef_37_2.bcp\n";
+
 $pull = XML::TreePuller->new(location => $ARGV[0]);
 $pull->reader;
 $pull->iterate_at('/ExchangeSet/Rs', 'subtree');
@@ -18,7 +34,10 @@ $pull->iterate_at('/ExchangeSet/Rs', 'subtree');
 open(FOUT, ">".$result_file) or die $!;
 open(FOUT_ERROR, ">".$error_file) or die $!;
 
+my $count = 0;
+
 while ($element = $pull->next) {
+	$count++;
 	$field_values{"rsId"} = "rs".$element->attribute("rsId");
 	my $hgvs_text = "";
 
@@ -34,47 +53,20 @@ while ($element = $pull->next) {
 		}
 	}
 
-	# need to loop through the Ss nodes, looking for an Ss/Sequence/Observed
-	# with the ref_allele/var_allele pair from the hgvs node, then take the
-	# last 15 chars of seq5 and the first 15 of seq3. If we can't find
-	# that, look for a reverse-complement ref_allele/var_allele pair and take
-	# the first 15 chars of seq5 and the last 15 chars of seq3
-
-	# check and see if the allele pairs are the same as or the reverse ...
-	# in either case, take the flanking sequences as normal
-	foreach my $Ss ($element->get_elements('Ss')) {
-		if ($Ss->get_elements("Sequence/Observed")->text eq $field_values{"var_allele"}."/".$field_values{"ref_allele"} ||
-			$Ss->get_elements("Sequence/Observed")->text eq $field_values{"ref_allele"}."/".$field_values{"var_allele"} ) {
-			$Ss->get_elements("Sequence/Seq5")->text =~ m/^.*(\w{15})$/; 
-			$field_values{"Seq5"} = uc $1;
-			$Ss->get_elements("Sequence/Seq3")->text =~ m/^(\w{15}).*$/;
-			$field_values{"Seq3"} = uc $1;
-			last;
+	# grab the sequence from the exemplarSs node; if the lookup against the hash
+	# built from b134_SNPChrPosOnRef_37_2.bcp has isReverse of 0, get flanking
+	# sequences as normal; if one, reverse complement and switch assignments
+	foreach my $Sequence ($element->get_elements("Sequence")) {
+		if (!defined($Sequence->attribute("exemplarSs"))) { next }
+		(my $seq5 = $Sequence->get_elements("Seq5")->text) =~ s/^.*(\w{15})$/$1/;
+		(my $seq3 = $Sequence->get_elements("Seq3")->text) =~ s/^(\w{15}).*$/$1/;
+		if (!$orientation_values{$field_values{"rsId"}}->{isReverse}) {
+			$field_values{"Seq5"} = uc $seq5;
+			$field_values{"Seq3"} = uc $seq3;
 		}
-	}
-
-	# the alleles are either complemented or both reversed and complemented ...
-	# so either complement or reverse and complement the flanking sequences, and switch
-	# the assignmentt, to reflect the fact that the read went the opposite way
-	if (!exists($field_values{"Seq5"}) and !exists($field_values{"Seq3"})) {
-		(my $complement_alleles = $field_values{"ref_allele"}."/".$field_values{"var_allele"}) =~ tr/ACTGactg/TGACtgac/;
-		foreach my $Ss ($element->get_elements('Ss')) {
-			if ($Ss->get_elements("Sequence/Observed")->text eq $complement_alleles) {
-				$Ss->get_elements("Sequence/Seq5")->text =~ m/^.*(\w{15})$/; 
-				($field_values{"Seq5"} = uc $1) =~ tr/ACTG/TGAC/;
-
-				$Ss->get_elements("Sequence/Seq3")->text =~ m/^(\w{15}).*$/;
-				($field_values{"Seq3"} = uc $1) =~ tr/ACTG/TGAC/;
-				last;
-			}
-			elsif ($Ss->get_elements("Sequence/Observed")->text eq reverse $complement_alleles) {
-				$Ss->get_elements("Sequence/Seq5")->text =~ m/^.*(\w{15})$/; 
-				($field_values{"Seq3"} = uc reverse $1) =~ tr/ACTG/TGAC/;
-
-				$Ss->get_elements("Sequence/Seq3")->text =~ m/^(\w{15}).*$/;
-				($field_values{"Seq5"} = uc reverse $1) =~ tr/ACTG/TGAC/;
-				last;
-			}
+		else {
+			($field_values{"Seq3"} = uc reverse $seq5) =~ tr/ACTG/TGAC/;
+			($field_values{"Seq5"} = uc reverse $seq3) =~ tr/ACTG/TGAC/;
 		}
 	}
 
@@ -113,6 +105,8 @@ while ($element = $pull->next) {
 	# clear out for the next iteration of the loop
 	%field_values = ();
 }
+
+print STDOUT "total xml Rs count: $count\n";
 
 close(FOUT);
 close(FOUT_ERROR);
