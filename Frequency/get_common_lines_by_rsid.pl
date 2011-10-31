d76330f1d5ebee8542bcea41253d3497e91243ea
p-qc <p-qc@stornext7.(none)>
2011-10-31 12:52:36 -0500

    Changed to have regex constructed from user input.

--- Frequency/get_common_lines_by_rsid.pl
+++ Frequency/get_common_lines_by_rsid.pl
@@ -1,14 +1,40 @@
+#!/hgsc_software/perl/latest/bin/perl
+
+use warnings;
+use strict;
+use diagnostics;
+
 if ($#ARGV < 2) { die "usage: perl get_common_rsids_from_annot.pl ".
 	"/path/to/file/to/match.txt ".
 	"/path/to/probelist.txt ".
-	"/path/to/output/file.txt" } 
+	"/path/to/output/file.txt ".
+	"\"separator\" ".
+	"zero_indexed_rsId_column_number\n";
+}
 
 my @rsIds; # will contain list of rsIds from file
+
+# build regex based on separator and column number inputs
+my $separator = $ARGV[3];
+my $rsid_col_number = $ARGV[4];
+
+my $regex = "^";
+foreach my $i (0..$rsid_col_number) {
+	my $regex_to_append = "[^".$separator."]+";
+	if ($i == $rsid_col_number) {
+		$regex_to_append = "(".$regex_to_append.")";
+	}
+	$regex_to_append .= $separator;
+	$regex .= $regex_to_append;
+}
+$regex .= ".*\$";
+
+print "using regex $regex\n";
 
 open(FIN, $ARGV[0]) or die $!;
 while (my $line = <FIN>) {
 	chomp($line);
-	$line =~ m/^[^\t]+\t[^\t]+\t([^\t]+)\t.*$/;
+	$line =~ m/$regex/;
 	push @rsIds, $1;
 }
 close(FIN);
