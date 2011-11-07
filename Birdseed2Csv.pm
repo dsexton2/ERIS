f79c82dc7865822cc06aad13797e50faa139e483
p-qc <p-qc@stornext7.(none)>
2011-11-07 13:29:12 -0600

    Removed tcga specific processing, fixed general processing.
    General processing now gets the self-concordance value based off of the
    SNP array name match in the Samples data structure matched against
    values of the line matching that SNP array in the birdseed.txt file.

--- Birdseed2Csv.pm
+++ Birdseed2Csv.pm
@@ -6,6 +6,9 @@ use diagnostics;
 use Log::Log4perl;
 use Concordance::Utils;
 
+if (!Log::Log4perl->initialized()) {
+	Log::Log4perl->init("/users/p-qc/dev_concordance_pipeline/Concordance/log4perl.cfg");
+}
 my $error_log = Log::Log4perl->get_logger("errorLogger");
 my $debug_log = Log::Log4perl->get_logger("debugLogger");
 
@@ -151,14 +154,17 @@ sub __base_output__ {
 
 	$debug_log->debug("Processing file $file\n");
 	$file =~ /^.*\/(.*?)\.birdseed\.txt$/;
-	my $name = $1;
+	my $file_name = $1;
+	#my $SNP_array_name = $1; # the file name
+	my $SNP_array_name = "";
 
 	# find the right Sample object by looking for the sample_id as all
-	# or part of the filename, then output run ID, sample ID
+	# or part of the SNP array name, then output run ID, sample ID
 	my %samples = %{ $self->samples };
 	foreach my $sample_id (keys %samples) {
-		if ($name =~ /$sample_id/) {
+		if ($file_name =~ /$sample_id/) {
 			print FOUT $samples{$sample_id}->run_id.",".$sample_id.",";
+			$SNP_array_name = $samples{$sample_id}->snp_array;
 		}
 	}
 
@@ -168,14 +174,22 @@ sub __base_output__ {
 
 	foreach my $avg (values %concordance) { $average += $avg }
 
+	my $self_concordance = "N/A";
+
+	if (defined($concordance{$SNP_array_name})) {
+		$self_concordance = $concordance{$SNP_array_name};
+	}
+
 	if ($num != 0) { $average = $average / $num }
 	else { $average = "NaN" }
-	print FOUT "$average";
+	print FOUT "$average,$self_concordance";
 
 	my $out_num=0;
+	# print out the first $MAX_PAIRS %concordance items, sorting in descending
+	# order of concordance value
 	if (scalar keys %concordance != 0) {
 		foreach my $key (sort { $concordance{$b} <=> $concordance{$a} } (keys(%concordance))) {
-			print FOUT ",$concordance{$key},$key";
+			print FOUT ",$key,$concordance{$key}";
 			$out_num++;
 			if ($out_num >= $MAX_PAIRS) { last; }
 		}
@@ -183,117 +197,6 @@ sub __base_output__ {
 	else { print FOUT ",No concordance values for $file" }
 	print $FOUT "\n";
 
-}
-
-=head3 __tcga_output__
-
- my $file = "/foo/bar.birdseed.txt";
- my $MAX_PAIRS = 5;
- open(FOUT, $self->output_csv_file);
- $self->__tcga_output__($file, $MAX_PAIRS, *FOUT);
-
-Private method to produce TCGA concordance values for C<Concordance::Judgement>.
-
-=cut
-
-sub __tcga_output__ {
-	my $self = shift;
-	my $file = shift;
-	my $MAX_PAIRS = shift;
-	my $FOUT = shift;
-
-	$file =~ /^.*\/(.*?)\.birdseed\.txt$/;
-	my $out1 = $1;
-
-	print FOUT "$out1";
-
-	my $out = 0;
-	my $out_num = 0;
-	my $SHO_NUM = $MAX_PAIRS;
-	my @out1_array=split(/[\_|\-]/,$out1);
-	my $match = "TCGA-".$out1_array[1]."-".$out1_array[2];
-	if ($out1_array[3] =~ /^(\d+)\D/) {
-		$out = $match."-".$1;
-	} else {
-		$out = $match."-".$out1_array[3];
-	}
-
-	my $one=""; my $one_num=-1;
-	my $slf=""; my $couple="";
-	my $average=0; my $num=0;
-	my %concordance=(); my %all=();
-	open(FIN, $file);
-	while(<FIN>) {
-		chomp;
-		if ($_ =~ /\//) {
-			print FOUT STDERR "ERROR :: $file is an angry birdseed file!!\n";
-			next;
-		} 
-		my @a=split(/\s+/);
-		$concordance{$a[0]}=$a[8];
-		$all{$a[0]}=$_;
-
-		$a[0] =~ /^.*?TCGA\-(.*?)\-(.*?)\-(\d+)\D\-(.*?)/;
-		my $mat1 = "TCGA-".$1."-".$2;
-		my $mat2 = "TCGA-".$1."-".$2."-".$3;
-
-		if($mat1 eq $match) {
-			if($mat2 eq $out) {
-				$slf=$_;
-			} else {
-				$couple=$_;
-			}
-		} else {
-			$average += $a[8];
-			$num++;
-		}
-
-		if($a[8] > $one_num) {
-			$one_num=$a[8];
-			$one=$_;
-		}
-
-	}
-
-	$average = $average / $num;
-
-	if ($slf =~ /^(.*?)_TCGA\-(.*?)$/) { $slf = "TCGA_".$2; }
-	if ($couple =~ /^(.*?)_TCGA\-(.*?)$/) { $couple = "TCGA_".$2; }
-
-	my @temp1 = ();
-	if ($slf ne "" && $couple ne "") {
-		@temp1=split(/\t/,$slf);
-		print FOUT ",$temp1[0],$average,$temp1[8]";
-		@temp1=split(/\t/,$couple);
-		print FOUT ",$temp1[8]";
-	} elsif($slf ne "") {
-		@temp1=split(/\t/,$slf);
-		print FOUT ",$temp1[0],$average,$temp1[8]";
-		print FOUT ",";
-	} elsif($couple ne "") {
-		print FOUT ",,$average,";
-		@temp1=split(/\t/,$couple);
-		print FOUT ",$temp1[8]";
-	} else {
-		print FOUT ",,$average,,";
-	}
-
-	$out_num=0;
-	foreach my $key (sort {$concordance{$b} <=> $concordance{$a}} keys %concordance) {
-		### print FOUT "$all{$key}\n";
-		$all{$key} =~ /^(.*?)_TCGA\-(.*?)$/;
-		my $temp="TCGA_".$2;
-		my @temp1=split(/\t/,$temp);
-		### print FOUT "TCGA_$2\n";
-		print FOUT ",$temp1[0],$temp1[8]";
-		$out_num++;
-
-		if($out_num >= $SHO_NUM) { last; }
-	}
-	print FOUT "\n";
-	#print "$out\t$one_num\n";
-	
-	close(FIN);	
 }
 
 =head3 execute
@@ -309,15 +212,8 @@ sub execute {
 	my $project_method = undef;
 	my $MAX_PAIRS = 0;
 
-	# some projects require different logic, so call the appropriate method
-	if ($slf->project_name =~ m/tcga/i) {
-		$project_method = "__tcga_output__";
-		$MAX_PAIRS = 6;
-	}
-	else {
-		$project_method = "__base_output__";
-		$MAX_PAIRS = 10;
-	}
+	$project_method = "__base_output__";
+	$MAX_PAIRS = 10;
 	
 	open(FOUT, ">".$slf->output_csv_file) or die $!;
 	my @files = Concordance::Utils->get_file_list($slf->path, "birdseed.txt");
