4cbb88fb3fa08c4fad7f9c4736f29945d4ade340
p-qc <p-qc@stornext7.(none)>
2011-11-16 08:19:18 -0600

    Using Sample container for processing, block until Moab jobs complete.
    Removed the input CSV file, everything is now passed in as the Sample
    container, which is a subset of the Sample container processed on by
    EGenoSolid which lacked CSFASTA files.  Also mimicked the dependency
    list processing of the Framework scripts to wait to return until the
    Moab submissions have completed.

--- Bam2csfasta.pm
+++ Bam2csfasta.pm
@@ -1,114 +1,5 @@
 package Concordance::Bam2csfasta;
 
-use strict;
-use warnings;
-use diagnostics;
-use Config::General;
-use Log::Log4perl;
-use Concordance::Common::Scheduler;
-
-my $error_log = Log::Log4perl->get_logger("errorLogger");
-my $debug_log = Log::Log4perl->get_logger("debugLogger");
-
-sub new {
-	my $self = {};
-	$self->{config} = undef;
-	$self->{csv_file} = undef;
-	$self->{samples} = undef;
-	$self->{debug_flag} = 0;
-	bless($self);
-	return $self;
-}
-
-sub config {
-	my $self = shift;
-	if (@_) { $self->{config} = shift }
-	return $self->{config};
-}
-
-sub csv_file {
-	my $self = shift;
-	if (@_) { $self->{csv_file} = shift }
-	return $self->{csv_file}; #\w+.csv$
-}
-
-sub samples {
-	my $self = shift;
-	if (@_) { $self->{samples} = shift }
-	return $self->{samples};
-}
-
-sub debug_flag {
-	my $self = shift;
-	if (@_) { $self->{debug_flag} = shift }
-	return $self->{debug_flag};
-}
-
-sub __submit__ {
-	my $self = shift;
-	my $sample_id = shift;
-	my $input_bam_file = shift;
-	my $fh = shift;
-	my %config = %{ $self->config };
-
-	if ($input_bam_file !~ /.bam$/) {
-		print "Bad sample_id/input_bam_file pair: $sample_id\t$input_bam_file\n";
-		next;
-	}
-	(my $output_csfasta_file = $input_bam_file) =~ s/bam$/csfasta/g;
-	my $command = "\"".$config{"java"}." -Xmx2G -jar ".$config{"bam_2_csfasta_jar"}.
-		" $input_bam_file".
-		" >".
-		" $output_csfasta_file \"";
-	print $fh "$sample_id,$output_csfasta_file\n";
-
-	my $scheduler = Concordance::Common::Scheduler->new;
-	$scheduler->command($command);
-	$scheduler->job_name_prefix($sample_id);
-	$scheduler->cores(2);
-	$scheduler->memory(2000);
-	$scheduler->priority("normal");
-	if (!$self->debug_flag) { $scheduler->execute }
-}
-
-sub execute {
-	my $self = shift;
-	my %config = %{ $self->config };
-	open(CSV_FILE_CSFASTA, "> ".$self->csv_file.".csfasta.csv");
-	my %samples = %{ $self->samples };
-	if (scalar keys %samples != 0) { # Sample objects passed from EGenoSolid
-		foreach my $sample_id (keys %samples) {
-			my $input_bam_file = $samples{$sample_id}->result_path;
-			$self->__submit__($sample_id, $input_bam_file, *CSV_FILE_CSFASTA);
-		}
-	}
-
-	else { # reading from a file
-		# error-checking
-		if ($self->csv_file !~ /.+\.csv$/) {
-			$error_log->error("csv_file must be of type *.csv: ".$self->csv_file."\n");
-			die "csv_file must be of type *.csv: ".$self->csv_file."\n";
-		}
-		if (!-e $self->csv_file) {
-			$error_log->error("csv_file DNE: ".$self->csv_file."\n");
-			die "csv_file DNE: ".$self->csv_file."\n";
-		}
-		open(CSV_FILE_BAM, $self->{csv_file});
-		while (<CSV_FILE_BAM>) {
-			chomp;
-			if($_ !~ /^(.*),+(.*)$/) { next; }
-			my $sample_id = $1;
-			my $input_bam_file = $2;
-			$self->__submit__($sample_id, $input_bam_file, *CSV_FILE_CSFASTA);
-		}
-		close(CSV_FILE_BAM);
-	}
-
-	close(CSV_FILE_CSFASTA);
-}
-
-1;
-
 =head1 NAME
 
 Concordance::Bam2csfasta - converts .bam files to .csfasta files
@@ -117,8 +8,8 @@ Concordance::Bam2csfasta - converts .bam files to .csfasta files
 
  use Concordance::Bam2csfasta;
  my $bam_2_csfasta = Concordance::Bam2csfasta->new;
- $bam_2_csfasta->config(%config);
- $bam_2_csfasta->csv_file("foo.csv");
+ $bam_2_csfasta->config(\%config);
+ $bam_2_csfasta->samples(\%samples);
  $bam_2_csfasta->convert_bam_to_csfasta;
 
 =head1 DESCRIPTION
@@ -130,32 +21,188 @@ sample ID / .csfasta path pairs.
 
 =head2 Methods
 
-=over 12
+=cut
 
-=item C<new>
+use strict;
+use warnings;
+use diagnostics;
+use Config::General;
+use Log::Log4perl;
+use Concordance::Common::Scheduler;
 
-Returns a new Concordance::Bam2csfasta object.
+my $error_log = Log::Log4perl->get_logger("errorLogger");
+my $debug_log = Log::Log4perl->get_logger("debugLogger");
+my $warn_log = Log::Log4perl->get_logger("warnLogger");
 
-=item C<config>
+=head3 new
 
-Gets and or sets the General::Config object.
+ my $bam2csfasta = Concordance::Bam2csfasta->new;
 
-=item C<csv_file>
+Returns a new Bam2csfasta instance.
 
-Gets and sets the CSV file containing sample ID / .bam path pairs.
+=cut
 
-=item C<convert_bam_to_csfasta>
+sub new {
+	my $self = {};
+	$self->{config} = undef;
+	$self->{samples} = undef;
+	$self->{debug_flag} = 0;
+	$self->{dependency_list} = undef;
+	bless($self);
+	return $self;
+}
 
-Iterates on the .bam files provided in the CSV and calls a JAR to convert each to a .csfasta.  Also writes sample ID / .csfasta path pairs to a new CSV file.
+=head3 config
 
-=back
+ $bam2csfasta->config(\%config);
+ my %config = %{ $self->config };
+
+Gets and sets the hash reference to the configuration items.
+
+=cut
+
+sub config {
+	my $self = shift;
+	if (@_) { $self->{config} = shift }
+	return $self->{config};
+}
+
+=head3 samples
+
+ $bam2csfasta->samples(\%samples);
+ my %samples = %{ $self->samples };
+
+Gets and sets the hash reference to the Sample container.
+
+=cut
+
+sub samples {
+	my $self = shift;
+	if (@_) { $self->{samples} = shift }
+	return $self->{samples};
+}
+
+=head3 debug_flag
+
+ $bam2csfasta->debug_flag(1);
+ if ($self->debug_flag) { print debug_info }
+
+Gets and sets the debug_flag, indicating whether or not to print relevant debug
+information, or to refrain from executing certain methods:
+
+=cut
+
+sub debug_flag {
+	my $self = shift;
+	if (@_) { $self->{debug_flag} = shift }
+	return $self->{debug_flag};
+}
+
+=head3 dependency_list
+
+ $scheduler->dependency_list($job_id);
+
+Gets and sets the dependency list for a job, which causes a job to delay execution
+until the jobs in its dependency list have completed.  When setting with this method, 
+job IDs are appended to the class member to form a colon-delimited list, which is 
+what msub expects.
+
+=cut
+
+sub dependency_list {
+	my $self = shift;
+	if (@_) { $self->{dependency_list} .= ":".shift }
+	return $self->{dependency_list};
+}
+
+=head3 __submit__
+
+ $self->__submit__(sample_id, input_bam_file, *FH;
+
+This private method is passed information from C<execute> to submit a job to
+Moab, which generates a CSFASTA file from the requisite BAM file.
+
+=cut
+
+sub __submit__ {
+	my $self = shift;
+	my $sample_id = shift;
+	my $input_bam_file = shift;
+	my %config = %{ $self->config };
+
+	if ($input_bam_file !~ /.bam$/) {
+		print "Bad sample_id/input_bam_file pair: $sample_id\t$input_bam_file\n";
+		next;
+	}
+	(my $output_csfasta_file = $input_bam_file) =~ s/bam$/csfasta/g;
+	my $command = "\"".$config{"java"}." -Xmx2G -jar ".$config{"bam_2_csfasta_jar"}.
+		" $input_bam_file".
+		" >".
+		" $output_csfasta_file \"";
+
+	my $scheduler = Concordance::Common::Scheduler->new;
+	$scheduler->command($command);
+	$scheduler->job_name_prefix($sample_id);
+	$scheduler->cores(2);
+	$scheduler->memory(2000);
+	$scheduler->priority("normal");
+	if (!$self->debug_flag) { $scheduler->execute }
+	
+	$self->dependency_list($scheduler->job_id);
+}
+
+=head3 execute
+
+ $bam2csfasta->execute;
+
+Method for use by the calling class to initiate processing after passing the
+necessary data via the accessor/mutator methods.  It will iterate on the Sample
+container and extract the necessary information for a Moab submission, which is
+accomplished by calling an internal method.
+
+=cut
+
+sub execute {
+	my $self = shift;
+	my %config = %{ $self->config };
+	my %samples = %{ $self->samples };
+	if (scalar keys %samples != 0) { # Sample objects passed from EGenoSolid
+		foreach my $sample (values %samples) {
+			$self->__submit__($sample->run_id, $sample->result_path);
+		}
+	}
+	my @dependency_list;
+	if (defined($self->dependency_list)) {
+		@dependency_list = split(/:/, $self->dependency_list);
+		$debug_log->debug("dependency list: @dependency_list\n");
+		if ($dependency_list[0] eq "") { splice(@dependency_list, 0, 1) }
+	}
+	else {
+		print "Warning: dependency_list undefined.  It's possible that no jobs were submitted\n";
+		$warn_log->warn("Warning: dependency_list undefined.  It's possible that no jobs were submitted\n");
+	}
+	print "Waiting for Bam2csfsta jobs to finish on msub...\n";
+	while (@dependency_list) {
+		foreach my $i (0..$#dependency_list) {
+			my $qstat_info = `qstat $dependency_list[$i]`;
+			if ($qstat_info !~ m/\bR\b/ and $qstat_info !~ m/\bQ\b/) {
+				print "Job ".$dependency_list[$i]." completed.\n";
+				$debug_log->debug("Job ".$dependency_list[$i]." completed.\n");
+				splice (@dependency_list, $i, 1);
+			}
+		}
+		if (scalar @dependency_list > 0) { sleep(600) }
+	}
+}
+
+1;
 
 =head1 LICENSE
 
-This script is the property of Baylor College of Medecine.
+GPLv3.
 
 =head1 AUTHOR
 
-Updated by John McAdams - L<mailto:mcadams@bcm.edu>
+John McAdams - L<mailto:mcadams@bcm.edu>
 
 =cut
