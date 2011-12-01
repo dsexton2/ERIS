1e3865ed71622f3b213172e0b239c53e1c520d6f
p-qc <p-qc@stornext7.(none)>
2011-12-01 14:32:35 -0600

    Added dependency_list member for job submissions, cleaned up, perldoc.

--- RawBsToGeli.pm
+++ RawBsToGeli.pm
@@ -1,26 +1,58 @@
 package Concordance::RawBsToGeli;
 
+=head1 NAME
+
+Concordance::RawBsToGeli 
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head2 Methods
+
+=cut
+
 use strict;
 use warnings;
+
+use Carp;
+use Concordance::Common::Scheduler;
+use Concordance::Utils;
 use Config::General;
 use Log::Log4perl;
-use Concordance::Common::Scheduler;
 
+if (!Log::Log4perl->initialized()) {
+	Log::Log4perl->init("/users/p-qc/dev_concordance_pipeline/Concordance/log4perl.cfg");            
+}
 my $error_log = Log::Log4perl->get_logger("errorLogger");
 my $debug_log = Log::Log4perl->get_logger("debugLogger");
+
+=head3 new
+
+ my $rbtg = Concordance::RawBsToGeli->new;
+
+Returns a new instance of RawBsToGeli.
+
+=cut
 
 sub new {
 	my $self = {};
 	$self->{config} = undef;
-	$self->{path} = undef;
-	$self->{sample_name} = undef;
-	$self->{snp60_definition} = undef;
-	$self->{sequence_dictionary_path} = undef;
-	$self->{reference_path} = undef;
-	$self->{output_likelihoods} = undef;
+	$self->{raw_birdseed_dir} = undef;
+	$self->{project_name} = undef;
+	$self->{dependency_list} = undef;
 	bless($self);
 	return $self;
 }
+
+=head3 config
+
+ $rbtg->config(\%config);
+ my $config_hashref = $self->config;
+
+Gets or sets the hash containing the configuration items.
+
+=cut
 
 sub config {
 	my $self = shift;
@@ -28,132 +60,100 @@ sub config {
 	return $self->{config};
 }
 
-sub path {
+=head3
+
+ $rbtg->raw_birdseed_dir("/foo/bar");
+ my $raw_birdseed_dir = $self->raw_birdseed_dir;
+
+Gets or sets the path to the directory containing the raw birdseed files.
+
+=cut
+
+sub raw_birdseed_dir {
 	my $self = shift;
-	if (@_) { $self->{path} = shift; }
-	return $self->{path}; #[^\0]+
+	if (@_) { $self->{raw_birdseed_dir} = shift; }
+	return $self->{raw_birdseed_dir};
 }
 
-sub sample_name {
+=head3 project_name
+
+ $rbtg->sample_name("foo");
+ my $sample_name = $rbtg->sample_name;
+
+Gets and sets the project name; this is required by the JAR.
+
+=cut
+
+sub project_name {
 	my $self = shift;
-	if (@_) { $self->{sample_name} = shift }
-	return $self->{sample_name}; #[^\0]+
+	if (@_) { $self->{project_name} = shift }
+	return $self->{project_name};
 }
 
-sub snp60_definition_path {
+=head3 dependency_list
+
+ $scheduler->dependency_list($job_id);
+
+Gets and sets the dependency list for a job, which causes a job to delay execution
+until the jobs in its dependency list have completed.  When setting with this method, 
+job IDs are appended to the class member to form a colon-delimited list, which is 
+what msub expects.
+
+=cut
+
+sub dependency_list {
 	my $self = shift;
-	if (@_) { $self->{snp60_definition_path} = shift }
-	return $self->{snp60_definition_path}; #\w+.csv$
+	if (@_) { $self->{dependency_list} .= ":".shift }
+	return $self->{dependency_list};
 }
 
-sub sequence_dictionary_path {
-	my $self = shift;
-	if (@_) { $self->{sequence_dictionary_path} = shift }
-	return $self->{sequence_dictionary_path}; #\w+.dict$
-}
+=head3 execute
 
-sub reference_path {
-	my $self = shift;
-	if (@_) { $self->{reference_path} = shift }
-	return $self->{reference_path}; #\w+.fasta$
-}
+ $rbtg->execute;
 
-sub output_likelihoods {
-	my $self = shift;
-	if (@_) { $self->{output_likelihoods} = shift }
-	return $self->{output_likelihoods}; #False|True
-}
+Generically named method to kick off processing.
 
-sub _get_file_list_ {
-	my $self = shift;
-	my $file_extension = "";
-	if (@_) { $file_extension = shift; }
-	my @files = glob($self->path."/*".$file_extension);
-	my $size = @files; 
-	if ($size == 0) {
-		$error_log->error("no ".$file_extension." files found in ".$self->path."\n");
-		exit;
-	}
-	return @files;
-}
+=cut
 
 sub execute {
 	my $self = shift;
-	my %config = %{ $self->config };
-	my @files = $self->_get_file_list_(".birdseed.data.txt");
-	my $cmd = '';
+	my $config = $self->config;
 
+	my @files = Concordance::Utils->get_file_list($self->raw_birdseed_dir, "birdseed.data.txt");
+	if ($#files == -1) { croak "No birdseed.data.txt files in ".$self->raw_birdseed_dir."\n" }
+
+	my $cmd = '';
 	foreach my $file (@files) {
 	# covert cancer birdseed data files to geli format
-		$cmd = "\"".$config{"java"}." -jar ".$config{"cancer_birdseed_snps_to_geli_jar"}.
-		" I=$file".
-		" S=".$self->sample_name.
-		" SNP60_DEFINITION=".$self->snp60_definition_path.
-		" SD=".$self->sequence_dictionary_path.
-		" R=".$self->reference_path.
-		" O=$file.geli \"";
+		$cmd = "\"".$config->{java}." -jar ".
+			$config->{cancer_birdseed_snps_to_geli_jar}.
+			" I=$file".
+			" S=".$self->project_name.
+			" SNP60_DEFINITION=".$config->{snp60_definition_path}.
+			" SD=".$config->{sequence_dictionary_path}.
+			" R=".$config->{reference_path}.
+			" O=$file.geli \"";
 		$file =~ /.*\/(.*)\.birdseed\.data\.txt$/;
 		my $scheduler = Concordance::Common::Scheduler->new;
 		$scheduler->command($cmd);
-		$scheduler->job_name_prefix($1."_toGELI");
+		$scheduler->job_name_prefix($1."_toGELI".$$);
 		$scheduler->cores(2);
 		$scheduler->memory(2000);
 		$scheduler->priority("normal");
 		$scheduler->execute;
+
+		$self->dependency_list($scheduler->job_id);
 	}
 }
 
-1;
-
-=head1 NAME
-
-Concordandce::BuildGeliAndBS - converts .birdseed.data.txt to .bs
-
-=head1 SYNOPSIS
-
- use Concordance::BuildGeliAndBS;
- my $build_Geli_and_BS = Concordance::BuildGeliAndBS->new;
- $build_Geli_and_BS->config(%config);
- $build_Geli_and_BS->path("/foo/bar/");
- $build_Geli_and_BS->build_geli;
- $build_Geli_and_BS->build_bs;
-
-=head1 DESCRIPTION
-
-This script converts all .birdseed.data.txt files in the target directory into .bs, using .geli as an intermediate format.  It does this by calling two JARs, which each accomplish one of the conversions.
-
-=head2 Methods
-
-=over 12
-
-=item C<new>
-
-Returns a new Concordance::BuildGeliAndBS object.
-
-=item C<config>
-
-Gets or sets and gets the General::Config object.
-
-=item C<path>
-
-Gets or sets and gets the path string in which to search for .birdseed.data.txt or .geli files.
-
-=item C<build_geli>
-
-Calls a JAR to convert each .birdseed.data.txt file to a .geli file.  
-
-=item C<build_bs>
-
-Calls a JAR to convert each .geli file into a .bs file.
-
-=back
-
 =head1 LICENSE
 
-This script is the property of Baylor College of Medicine.
+GPLv3.
 
 =head1 AUTHOR
 
-Updated by John McAdams - L<mailto:mcadams@bcm.edu>
+John McAdams - L<mailto:mcadams@bcm.edu>
 
 =cut
+
+1;
