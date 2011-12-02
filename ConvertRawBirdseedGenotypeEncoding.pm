db0b5dd05fb38a9549b4d3c44a5ae1df04bbcb83
p-qc <p-qc@stornext7.(none)>
2011-12-02 11:19:29 -0600

    Initial commits.
    Refactored Change_AA_to_0; broke it out into a module and a script. The
    module submits jobs to moab, which use the script.  Processing on moab >
    processing locally.

---
+++ ConvertRawBirdseedGenotypeEncoding.pm
@@ -0,0 +1,46 @@
+package Concordance::Change_AA_to_0;
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Carp;
+use Concordance::Common::Scheduler;
+use File::Basename;
+use Log::Log4perl;
+
+if (!Log::Log4perl->initialized()) {
+	Log::Log4perl->init("/users/p-qc/dev_concordance_pipeline/Concordance/log4perl.cfg");            
+}
+my $error_log = Log::Log4perl->get_logger("errorLogger");
+my $debug_log = Log::Log4perl->get_logger("debugLogger");
+
+sub new {
+	my $self = {};
+	$self->{path} = undef;
+	bless($self);
+	return $self;
+}
+
+sub path {
+	my $self = shift;
+	if (@_) { $self->{path} = shift; }
+	return $self->{path};
+}
+
+sub execute {
+	my $self = shift;
+	my @files=glob($self->path."/*.txt");
+
+	foreach my $file (@files) {
+		my $cmd = "perl /users/p-qc/dev_concordance_pipeline/convert_raw_birdseed_genotype_encoding.pl $file";
+		my $scheduler = Concordance::Common::Scheduler->new;
+		$scheduler->command($cmd);
+		$scheduler->job_name_prefix(basename($file)."_toGELI".$$);
+		$scheduler->cores(2);
+		$scheduler->memory(2000);
+		$scheduler->priority("normal");
+		$scheduler->execute;
+		$self->dependency_list($scheduler->job_id);
+	}
+}
---
+++ convert_raw_birdseed_genotype_encoding.pl
@@ -0,0 +1,62 @@
+#!/hgsc_software/perl/latest/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Carp;
+use Log::Log4perl;
+
+if (@ARGV != 1) {
+	croak "usage: convert_raw_birdseed_genotype_encoding.pl ".
+	"/path/to/raw_birdseed_file/to/convert ".
+	"\n";
+}
+
+if (!Log::Log4perl->initialized()) {
+	Log::Log4perl->init("/users/p-qc/dev_concordance_pipeline/Concordance/log4perl.cfg");            
+}
+my $error_log = Log::Log4perl->get_logger("errorLogger");
+my $debug_log = Log::Log4perl->get_logger("debugLogger");
+
+my $raw_bs_file = $ARGV[0];
+
+if (!-e $raw_bs_file) {
+	$error_log->error("No such file: $raw_bs_file\n");
+	croak "No such file: $raw_bs_file\n";
+}
+
+$debug_log->debug("Processing file: $raw_bs_file\n");
+
+open(FIN, $raw_bs_file) or croak $!;
+(my $outfile = $raw_bs_file) =~ s/(\.txt)$/\.birdseed\.data\.txt/;
+open(FOUT,">".$outfile) or croak $!;
+
+while (my $line = <FIN>) {
+	chomp($line);
+	if ($line =~ m/^(#|Probe)/) {
+		print FOUT $line."\n";
+	}
+	else {
+		my @tab_delimited_cols = split(/\t/, $line);
+		if (@tab_delimited_cols != 3) {
+			$error_log->error("1\t".(scalar @tab_delimited_cols)."\t$raw_bs_file\n");
+			next;
+		}
+		if ($tab_delimited_cols[1] eq "AA") {
+			print FOUT "$tab_delimited_cols[0]\t0\t$tab_delimited_cols[2]\n";
+		}
+		elsif ($tab_delimited_cols[1] eq "AB") {
+			print FOUT "$tab_delimited_cols[0]\t1\t$tab_delimited_cols[2]\n";
+		}
+		elsif ($tab_delimited_cols[1] eq "BB") {
+			print FOUT "$tab_delimited_cols[0]\t2\t$tab_delimited_cols[2]\n";
+		}
+		else {
+			$error_log->error("$tab_delimited_cols[0]\t$tab_delimited_cols[1]\t$raw_bs_file\n");
+		}
+	}
+}
+
+close(FIN) or carp $!;
+close(FOUT) or carp $!;
