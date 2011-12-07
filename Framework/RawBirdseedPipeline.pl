24e66e4e2f1abe22e226e9179bc48500ec1bb9ae
p-qc <p-qc@stornext7.(none)>
2011-12-07 11:32:09 -0600

    Refactored to use Getopt::Long for arguments, podusage for help.

--- Framework/RawBirdseedPipeline.pl
+++ Framework/RawBirdseedPipeline.pl
@@ -9,14 +9,10 @@ use Concordance::Bs2birdseed;
 use Concordance::ConvertRawBirdseedGenotypeEncoding;
 use Concordance::GeliToBs;
 use Concordance::RawBsToGeli;
+use Getopt::Long;
+use Pod::Usage;
 
-if (scalar @ARGV != 3) {
-	die "Usage: perl RawBirdseedPipeline.pl ".
-		"/path/to/config/file ".
-		"/path/to/raw_birdseed_dir ".
-		"project_name ".
-		"\n";
-}
+pod2usage(-exitstatus => 0) if (@ARGV == 0);
 
 if (!Log::Log4perl->initialized()) {
 	Log::Log4perl->init("/users/p-qc/dev_concordance_pipeline/Concordance/log4perl.cfg");            
@@ -25,24 +21,45 @@ my $debug_log = Log::Log4perl->get_logger("debugLogger");
 my $debug_to_screen = Log::Log4perl->get_logger("debugScreenLogger");
 my $error_log = Log::Log4perl->get_logger("errorLogger");
 
-my $config_file_path = $ARGV[0];
-my $raw_birdseed_dir = $ARGV[1];
-my $project_name = $ARGV[2];
+my $help, my $man;
+my $config_file_path = '';
+my $raw_birdseed_dir = '';
+my $project_name = '';
+my $convert_geno_enc_flag = '';
 
-if (!-e $config_file_path) { croak $! }
-if (!-e $raw_birdseed_dir) { croak $! }
+GetOptions
+	(
+		'help|?' => \$help,
+		man => \$man,
+		'config=s' => \$config_file_path, 
+		'raw_bs_dir=s' => \$raw_birdseed_dir,
+		'project=s' => \$project_name,
+		'convert-enc' => \$convert_geno_enc_flag
+	) or pod2usage(1);
+
+pod2usage(1) if $help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+
+pod2usage(-exitstatus => 0, -verbose => 1) if (!$config_file_path or !$raw_birdseed_dir or !$project_name);
+
+if (!-e $config_file_path) { croak $config_file_path.": ".$! }
+if (!-e $raw_birdseed_dir) { croak $raw_birdseed_dir.": ".$! }
 
 my %config = new Config::General($config_file_path)->getall;
 
-print "Converting Illumina genotype encoding to birdseed encoding ... \n";
-my $convert_geno_enc = Concordance::ConvertRawBirdseedGenotypeEncoding->new;
-$convert_geno_enc->path($raw_birdseed_dir);
-$convert_geno_enc->execute;
+my @dependency_list;
 
-my @dependency_list = split(/:/, $convert_geno_enc->dependency_list);
-if (@dependency_list != 0) {
-	$debug_log->debug("RawBsToGeli dependency list: @dependency_list\n");
-	&wait_for_jobs_to_finish("RawBsToGeli", \@dependency_list);
+if ($convert_geno_enc_flag) {
+	print "Converting Illumina genotype encoding to birdseed encoding ... \n";
+	my $convert_geno_enc = Concordance::ConvertRawBirdseedGenotypeEncoding->new;
+	$convert_geno_enc->path($raw_birdseed_dir);
+	$convert_geno_enc->execute;
+
+	my @dependency_list = split(/:/, $convert_geno_enc->dependency_list);
+	if (@dependency_list != 0) {
+		$debug_log->debug("RawBsToGeli dependency list: @dependency_list\n");
+		&wait_for_jobs_to_finish("RawBsToGeli", \@dependency_list);
+	}
 }
 
 print "Converting raw birdseed files to geli files ... \n";
@@ -93,3 +110,56 @@ sub wait_for_jobs_to_finish {
 		if (scalar @$dependency_list > 0) { sleep(600) }
 	}
 }
+
+=head1 NAME
+
+RawBirdseedPipeline - prepare raw birdseed files for concordance analysis
+
+=head1 SYNOPSIS
+
+RawBirdseedPipeline.pl [options] [file ...]
+
+Options:
+-config			path to the configuration path
+-raw_bs_dir		path to the directory containing the raw birdseed files
+-project		the name of the project for these birdseed files
+-convert-enc	option to convert genotype encoding
+-help			brief help message
+-man			full documentation
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-config>
+
+The path to the configuration file.
+
+=item B<-raw_bs_dir>
+
+The path to the directory containing the raw birdseed files to be processed.
+
+=item B<-project>
+
+The name of the project to which these birdseed files belong.
+
+=item B<-convert-enc>
+
+Convert the genotype encoding prior to processing the raw birdseed files.
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+B<RawBirdseedPipeline> will process a directory of raw birdseed files to prepare
+them for use by the concordance analysis pipeline.
+
+=cut
